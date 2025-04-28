805fa4797e1a29110e8e6d553e263cdad7529378
강성민 <sprtmsfhtk@naver.com>
2025-04-28 10:03:05 +0900

    메모 생성 테스트, 지하실 최대 크기 조절을 위한 지하실 공통 태그 base 추가

--- capstone_map/Assets/Map_data/Map_Scripts/MemoManager.cs
+++ capstone_map/Assets/Map_data/Map_Scripts/MemoManager.cs
@@ -1,61 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Linq; // for .OrderBy
+using System.Collections;
+using System.Linq;
 
 public class MemoManager : MonoBehaviour
 {
-    public GameObject prefabToSpawn; // 추가할 프리팹
-    public int requiredCount = 3;    // 기다릴 최소 오브젝트 수
-    public int selectCount = 3;      // 무작위로 뽑을 수
-    public float checkInterval = 0.5f; // 체크 주기 (초)
     GameObject map;
+    MemoSpawnPoint[] memoSpawn;
+    public GameObject prefabToSpawn; // 생성할 메모 프리팹
 
-    void Start()
+    int memoCount = 3;
+
+    IEnumerator Start()
     {
         if (map == null)
         {
             map = GameObject.Find("Generated Map");
         }
-        StartCoroutine(WaitAndSpawn());
+        yield return new WaitForSeconds(1.0f);
+
+        memoSpawn = map.GetComponentsInChildren<MemoSpawnPoint>();
+        Debug.Log("메모 스폰 갯수 : " + memoSpawn.Length);
+
+        if (memoSpawn.Length >= memoCount)
+        {
+            Debug.Log("메모 스폰 충분");
+
+            int memoNum = 1;
+
+            // memoCount 만큼 랜덤으로 뽑아서 메모 생성
+            var selectedTargets = memoSpawn
+                .OrderBy(x => Random.value) // 랜덤으로 섞기
+                .Take(memoCount)           // memoCount 만큼 선택
+                .ToArray();
+
+            if (prefabToSpawn != null)
+            {
+                foreach (var target in selectedTargets)
+                {
+                    // 선택된 위치에 메모 생성
+                    GameObject newMemo = Instantiate(prefabToSpawn, target.transform.position, Quaternion.identity);
+                    newMemo.transform.SetParent(target.transform);  // 메모가 MemoSpawnPoint의 자식이 되도록 설정
+                    Debug.Log("메모 생성: " + newMemo.name + " at position " + target.transform.position);
+                    newMemo.GetComponentInChildren<Memo>().memoNumber = memoNum++;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("메모 스폰 부족");
+        }
     }
 
-    IEnumerator WaitAndSpawn()
+    // Update is called once per frame
+    void Update()
     {
-        while (true)
-        {
-            var targets = map.GetComponentsInChildren<MemoSpawnPoint>();
-            if (targets.Length >= requiredCount)
-                break;
 
-            yield return new WaitForSeconds(checkInterval);
-        }
-
-        var allTargets = map.GetComponentsInChildren<MemoSpawnPoint>().ToList();
-
-        var selectedTargets = allTargets
-            .OrderBy(x => Random.value)
-            .Take(selectCount)
-            .ToArray();
-
-        for (int i = 0; i < selectedTargets.Length; i++)
-        {
-            var target = selectedTargets[i];
-
-            GameObject newObj = Instantiate(prefabToSpawn, target.transform);
-            newObj.transform.localPosition = Vector3.zero;
-
-            var memoCom = newObj.GetComponentInChildren<Memo>();
-            if (memoCom != null)
-            {
-                memoCom.memoNumber = i;
-            }
-            else
-            {
-                Debug.LogWarning("newObj(혹은 그 자식들)에 Memo 컴포넌트가 없습니다!");
-            }
-
-        }
     }
-
 }
