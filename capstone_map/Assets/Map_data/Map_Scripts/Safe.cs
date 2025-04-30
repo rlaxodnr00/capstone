377f68f0a53e85a208b0c88638e610b858823f7a
강성민 <sprtmsfhtk@naver.com>
2025-04-30 11:23:25 +0900

    지하실 탈출구 프리팹 추가, 금고 프리팹 추가

---
+++ capstone_map/Assets/Map_data/Map_Scripts/Safe.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Safe : Interactable
+{
+    Animator animator;
+    AudioSource audioS;
+    public AudioClip clip;
+
+    private void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+        audioS = GetComponentInParent<AudioSource>();
+    }
+
+    public override void OnLookAt() { }
+
+    public override void OnLookAway() { }
+
+    public override void OnInteract()
+    {
+        // 문이 열려있거나 잠기지 않았을 때
+        if (animator.GetBool("open") || !animator.GetBool("locked"))
+        {
+            animator.SetBool("open", !animator.GetBool("open"));
+        }
+
+        // 효과음이 있으면 재생
+        if (audioS != null && clip != null)
+        {
+            audioS.PlayOneShot(clip);
+        }
+
+    }
+}
