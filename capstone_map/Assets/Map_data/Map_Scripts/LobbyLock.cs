2e34098fc49eb9fc3241eddc10e204bae47b7ffc
강성민 <sprtmsfhtk@naver.com>
2025-05-19 20:59:58 +0900

    테스트

--- capstone_map/Assets/Map_data/Map_Scripts/LobbyLock.cs
+++ capstone_map/Assets/Map_data/Map_Scripts/LobbyLock.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class LobbyLock : MonoBehaviour
 {
-    public Door[] lobbyDoors;
+    // public Door[] lobbyDoors;
 
-    void Start()
-    {
-        foreach (Door o in lobbyDoors)
-        {
-            o.Doorlock(true);
-        }
-    }
+    // void Start()
+    // {
+    //     foreach (Door o in lobbyDoors)
+    //     {
+    //         o.Doorlock(true);
+    //     }
+    // }
 }
