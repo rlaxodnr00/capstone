57a20381e2bc4be5527bd009354acb3bf53a16e9
강성민 <sprtmsfhtk@naver.com>
2025-06-09 09:24:30 +0900

    LobbyDoor 클리어 로직 수정 Thunder 프리팹 AudioSource 추가 천둥 소리 파일 추가

--- capstone_map/Assets/Map_data/Map_Scripts/LobbyDoor.cs
+++ capstone_map/Assets/Map_data/Map_Scripts/LobbyDoor.cs
@@ -32,7 +32,7 @@ public class LobbyDoor : Door
         if (hasCleared) return;
 
         // 문이 잠겼고 열쇠가 있으면
-        if (animator.GetBool("locked") && isKeyHeld(inven.heldItems ,inven.CurrentSlot))
+        if (animator.GetBool("locked") && isKeyHeld(inven.heldItems, inven.CurrentSlot))
         {
             // 열쇠를 소모하고?
             Debug.Log("문 열림");
@@ -40,16 +40,22 @@ public class LobbyDoor : Door
             animator.SetBool("locked", false);
             // 양쪽을 같이 연다.
             otherDoor.animator.SetBool("locked", false);
-
-            //게임 클리어
-            hasCleared = true;
-            StartCoroutine(DelayThenClear());
         }
         // 문이 잠기지 않았거나 열쇠가 없으면 기존 코드로 동작
         else
         {
             base.OnInteract();
             otherDoor.InteractWith();
+
+            // 문과 상호작용했을 때 문이 잠기지 않았다면 클리어
+            if (!animator.GetBool("locked"))
+            {
+                //게임 클리어
+                hasCleared = true;
+                // 다른 문도 똑같이 적용
+                otherDoor.hasCleared = true;
+                StartCoroutine(DelayThenClear());
+            }
         }
     }
 
@@ -60,6 +66,7 @@ public class LobbyDoor : Door
 
     public bool isKeyHeld(GameObject[] inven, int slot)
     {
+        if (inven[slot] == null) return false;
         Key key = inven[slot].GetComponent<Key>();
 
         if (key != null) return true;
