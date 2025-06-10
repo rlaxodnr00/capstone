6587fed4607a1279abe057969b56b10ddaa87513
reincarnation19 <laohusgwdr@gmail.com>
2025-06-10 12:13:53 +0900

    1. 몬스터가 오리 소리 못 듣는 현상 수정 2. 비상 탈출구 상호작용 시 게임 클리어

--- capstone_map/Assets/Map_data/Map_Scripts/BaseDoor.cs
+++ capstone_map/Assets/Map_data/Map_Scripts/BaseDoor.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
-
+using System.Collections;
 public class BaseDoor : Door
 {
     GameObject player;
     PlayerInventory inven;
 
+    HPController hp;
+
+    private bool hasCleared = false;
     private void Start()
     {
         animator = GetComponent<Animator>();
         player = GameObject.Find("WomanWarrior");
         inven = player.GetComponent<PlayerInventory>();
+
+        hp = player.GetComponent<HPController>();
 
         // 주석 해제 하면 잠긴 상태로 시작
         // animator.SetBool("locked", true);
@@ -32,6 +37,14 @@ public class BaseDoor : Door
         {
             Debug.Log("기존 코드로 동작");
             base.OnInteract();
+
+            // 문과 상호작용했을 때 문이 잠기지 않았다면 클리어
+            if (!animator.GetBool("locked"))
+            {
+                //게임 클리어
+                hasCleared = true;
+                StartCoroutine(DelayThenClear());
+            }
         }
     }
     
@@ -43,5 +56,11 @@ public class BaseDoor : Door
 
         if (mask != null) return true;
         return false;
+    }
+
+    private IEnumerator DelayThenClear()
+    {
+        yield return null; // 다음 프레임까지 Animator가 상태 전환할 수 있도록 대기
+        hp.CLEAR();
     }
 }
